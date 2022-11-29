edc995cf03a6d9f20d06352094c164cec7333faa
Junaid Ali <junaidalibacha@gmail.com>
2022-11-29 20:08:43 +0500

    ui completed

---
+++ lib/screens/score/score_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+
+import '../../constants.dart';
+import '../../controllers/question_controller.dart';
+
+class ScoreScreen extends StatelessWidget {
+  const ScoreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    QuestionController qnController = Get.put(QuestionController());
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          SvgPicture.asset("assets/icons/bg.svg", fit: BoxFit.fill),
+          Column(
+            children: [
+              const Spacer(flex: 3),
+              Text(
+                "Score",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline3!
+                    .copyWith(color: kSecondaryColor),
+              ),
+              const Spacer(),
+              Text(
+                "${qnController.correctAns * 10}/${qnController.questions.length * 10}",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline4!
+                    .copyWith(color: kSecondaryColor),
+              ),
+              const Spacer(flex: 3),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
