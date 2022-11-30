75ee18ffe20e4aa60d0ef700a872199034cc93f2
Junaid Ali <junaidalibacha@gmail.com>
2022-11-30 12:12:52 +0500

    minor changes

--- lib/screens/score/score_screen.dart
+++ lib/screens/score/score_screen.dart
@@ -12,7 +12,35 @@ class ScoreScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     QuestionController qnController = Get.put(QuestionController());
     return Scaffold(
-      body: Stack(
+      body:
+          // Container(
+          //   height: double.infinity,
+          //   width: double.infinity,
+          //   decoration: const BoxDecoration(image: DecorationImage(image: AssetImage(''),)),
+          //   child: Column(
+          //       children: [
+          //         const Spacer(flex: 3),
+          //         Text(
+          //           "Score",
+          //           style: Theme.of(context)
+          //               .textTheme
+          //               .headline3!
+          //               .copyWith(color: kSecondaryColor),
+          //         ),
+          //         const Spacer(),
+          //         Text(
+          //           "${qnController.correctAns * 10}/${qnController.questions.length * 10}",
+          //           style: Theme.of(context)
+          //               .textTheme
+          //               .headline4!
+          //               .copyWith(color: kSecondaryColor),
+          //         ),
+          //         const Spacer(flex: 3),
+          //       ],
+          //     ),
+          // ),
+
+          Stack(
         fit: StackFit.expand,
         children: [
           SvgPicture.asset("assets/icons/bg.svg", fit: BoxFit.fill),
